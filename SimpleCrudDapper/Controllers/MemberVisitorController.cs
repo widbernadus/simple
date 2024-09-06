4d67dacccef4cea5c21e72005a053bd45732e428
Bernadus Widaryanto <bernadus.widaryanto@uajy.ac.id>
2024-09-06 12:01:10 +0700

    add input visitor

---
+++ SimpleCrudDapper/Controllers/MemberVisitorController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleCrudDapper.DAO;
+
+namespace SimpleCrudDapper.Controllers
+{
+    public class MemberVisitorController : Controller
+    {
+        MemberVisitorDAO _dao;
+
+        public MemberVisitorController()
+        {
+            _dao = new MemberVisitorDAO();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult StoreVisitorData(string code)
+        {
+            int insert = _dao.InsertVisitor(code);
+            if (insert == 0) {
+                return Ok(new
+                {
+                    response = "Error",
+                    message = "Member not found!"
+                });
+            }
+            else
+            {
+                return Ok(new
+                {
+                    response = "Ok",
+                    message = "Successfully!"
+                });
+            }
+            
+        }
+
+        public IActionResult GetAllData() {
+            List<object> data = _dao.GetAllData();
+            return Ok(data);
+        }
+    }
+}
---
+++ SimpleCrudDapper/DAO/MemberVisitorDAO.cs
@@ -0,0 +1,56 @@
+﻿
+using Dapper;
+using SimpleCrudDapper.Models;
+using SimpleCrudDapper.Utility;
+
+namespace SimpleCrudDapper.DAO
+{
+    public class MemberVisitorDAO
+    {
+        private DbContext _context;
+        public MemberVisitorDAO()
+        {
+            _context = new DbContext();
+        }
+        public int InsertVisitor(string code)
+        {
+            var query = @"INSERT visitor (member_id, visiting_date)
+                        SELECT id, GETDATE() FROM member WHERE member_code = @CODE;
+                        SELECT @@ROWCOUNT AS INSERTED";
+
+            var param = new { CODE = code };
+            using (var connection = _context.CreateConnection1())
+            {
+                try
+                {
+                    var data = connection.QueryFirstOrDefault<int>(query, param);
+                    return data;
+                }
+                catch (Exception ex)
+                {
+                    return 0;
+                }
+            }
+        }
+
+        public List<object> GetAllData()
+        {
+            var query = @"SELECT visiting_date, member_code, M.name FROM visitor V
+                          LEFT JOIN member M ON M.id = V.member_id";
+
+            var param = new { };
+            using (var connection = _context.CreateConnection1())
+            {
+                try
+                {
+                    var data = connection.Query(query, param).ToList();
+                    return data;
+                }
+                catch (Exception ex)
+                {
+                    return new List<object>();
+                }
+            }
+        }
+    }
+}
